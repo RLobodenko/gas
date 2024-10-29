f017ecf3441ec8c4b42f89eb66198df1177e6ec8
Roman <47541355+RLobodenko@users.noreply.github.com>
2024-10-29 03:14:25 +0300

    Add files via upload

--- 6_en.js
+++ 6_en.js
@@ -5,20 +5,18 @@ var but1 = document.getElementById('1');
 
 function f1(){
     
-   var a = document.getElementById('in1').value;
-    var b = document.getElementById('in2').value;
-    var c = document.getElementById('in3').value;
-   
+   let a = document.getElementById('in1').value;
+    let b = document.getElementById('in2').value;
+    let c = document.getElementById('in3').value;
     
-    
-    //var d = 3.87 - a;
-   // var e = c - d;
-    var f = 0.13 + a + b + c;
-    var g = a + b + c;
-       // document.getElementById('out1').innerHTML = d.toFixed(2) + " м";
-       // document.getElementById('out2').innerHTML = e.toFixed(2) + " м";
-    document.getElementById('out3').innerHTML = f + " м";
-    document.getElementById('out4').innerHTML = g + " м";
+   a=parseFloat(a);
+   b=parseFloat(b);
+    c=parseFloat(c);
+    let j = a + b + c;
+     let k = 0.13 + a + b + c;
+  
+    document.getElementById('out3').innerHTML = j + " м";
+    document.getElementById('out4').innerHTML = k + " м";
 }
 
 
