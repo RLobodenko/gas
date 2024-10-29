7c8027b641e95e8ce75321a3344d0565a692e328
Roman <47541355+RLobodenko@users.noreply.github.com>
2024-10-29 04:30:59 +0300

    Update 6_en.js

--- 6_en.js
+++ 6_en.js
@@ -14,9 +14,14 @@ function f1(){
     c=parseFloat(c);
     let j = a + b + c;
      let k = 0.13 + a + b + c;
+      let l = 3.87 - a;
+     let m = c - l;
   
     document.getElementById('out3').innerHTML = j + " м";
     document.getElementById('out4').innerHTML = k + " м";
+    document.getElementById('out5').innerHTML = l + " м";
+    document.getElementById('out6').innerHTML = m + " м";
+    
 }
 
 
