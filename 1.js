9cee1dcc077be17238e01b1b4503c62f54e0615c
Roman <47541355+RLobodenko@users.noreply.github.com>
2025-01-25 19:16:03 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -3,11 +3,11 @@ var but1 = document.getElementById('1');
 function f1(){
     
     var a = document.getElementById('in1').value;
-
+ var b = document.getElementById('in2').value;
   var c = document.getElementById('in3').value;
    
 
-     var b = parseFloat(c + 1.87);
+     
     
     var e = a * c / b ;
 
