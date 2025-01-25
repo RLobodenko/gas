4fe188af59d500d394c1064e4151ec4b3836506e
Roman <47541355+RLobodenko@users.noreply.github.com>
2025-01-25 19:51:31 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -3,12 +3,13 @@ var but1 = document.getElementById('1');
 function f1(){
     
     var a = document.getElementById('in1').value;
+      var b = document.getElementById('in2').value;
   var c = document.getElementById('in3').value;
      var d = document.getElementById('in4').value;
 
      
     
-    var e = a * c / (c + d) ;
+    var e = a * c / b ;
 
    var f = a - e;
    
