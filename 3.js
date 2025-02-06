137a4c642bb2e15ffae2e4d0e02c6f18bac2c211
Roman <47541355+RLobodenko@users.noreply.github.com>
2025-02-06 06:11:19 +0300

    Update 3.js

--- 3.js
+++ 3.js
@@ -3,19 +3,16 @@ var but1 = document.getElementById('1');
 function f1(){
     
     var a = document.getElementById('in1').value;
-    var b = document.getElementById('in2').value;
-      
-  var c = document.getElementById('in3').value;
-     var d = document.getElementById('in4').value;
+    
 
      
     
-    var e = a * c / b ;
+    var b = 3.87 - a ;
 
-   var f = a - e;
    
-    document.getElementById('out3').innerHTML = e.toFixed(2) + " руб";
-        document.getElementById('out4').innerHTML = f.toFixed(2) + " руб";
+   
+    document.getElementById('out3').innerHTML = b.toFixed(2) + " м";
+      
    
     
 }
