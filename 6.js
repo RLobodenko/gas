f284ba96db48f5f7b8521ebd195d409efec331d9
Roman <47541355+RLobodenko@users.noreply.github.com>
2025-08-07 19:40:08 +0300

    Add files via upload

--- 6.js
+++ 6.js
@@ -5,6 +5,8 @@ var radio2 = document.getElementsByName('in2');
 var radio5 = document.getElementsByName('in5');
 var radio6 = document.getElementsByName('in6');
 
+var one = document.getElementById('1');
+
 
 
 function f2() {
@@ -19,7 +21,7 @@ function f2() {
  if(radio1[i].selected && radio5[i].selected) {
 
  document.getElementById('out1').innerHTML = "10кПа";
-                document.getElementById('out').innerHTML =  "0,74кПа";
+                document.getElementById('out').innerHTML =  "0,74 кПа";
                 return;
 
 
@@ -30,7 +32,7 @@ function f2() {
  else if(radio1[i].selected && radio6[i].selected) {
 
  document.getElementById('out1').innerHTML = "10кПа";
-                document.getElementById('out').innerHTML =  "0,77кПа";
+                document.getElementById('out').innerHTML =  "0,77 кПа";
                 return;
 
 
@@ -47,7 +49,7 @@ function f2() {
          else if(radio2[i].selected && radio5[i].selected) {
 
   document.getElementById('out1').innerHTML = "300кПа";
-                document.getElementById('out').innerHTML =  "17,71кПа";
+                document.getElementById('out').innerHTML =  "17,71 кПа";
                 return;
 
 
@@ -57,7 +59,7 @@ function f2() {
          else if(radio2[i].selected && radio6[i].selected) {
  document.getElementById('out1').innerHTML = "300кПа";
  
-                document.getElementById('out').innerHTML =  "18,46кПа";
+                document.getElementById('out').innerHTML =  "18,46 кПа";
                 return;
 
 
@@ -76,5 +78,35 @@ function f2() {
 
 
 
+function f1(){
+    
+    var a = document.getElementById('in7').value;
+    var b = document.getElementById('in8').value;
+      
+  var c = document.getElementById('in9').value;
+     var d = document.getElementById('in10').value;
+
+     
+    
+    var e = 676 * a + 729 * b + 2500 * c + 2641.96 * d;
+
+var f = 26 * a + 27 * b + 50 * c + 51.4 * d;
+    
+    var j = e / f ;
+
+ 
+            var k = 20 / j;
+            document.getElementById('out2').innerHTML = k.toFixed(2) + " кПа";
+    
+    var l = 480 / j
+    document.getElementById('out3').innerHTML = j.toFixed(2) + " кПа";
+       
+    }
+ 
+  
+
+     
+   
+    
 
 
